fbc21a6a350639942c89d718013923cbf04a29e6
Maurice Muli <147836099+Muli4@users.noreply.github.com>
2024-05-03 17:17:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// header section
+
+//scroll to section
+document.querySelectorAll('a[href^="#]').forEach(anchor =>
+    {
+        anchor.addEventListener('click',function(e) {
+            e.preventDefault();
+
+            document.querySelectorAll(this.getAttribute('href')).scrollIntoView({
+                behavior:'smooth'
+        });
+        });
+    });
