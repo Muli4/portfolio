f690e7049f833457e24d28436a488971f134672c
Muli4 <mulimaurice730@gmail.com>
2025-02-22 14:43:16 +0300

    deleted the old created a new one

---
+++ components/navbar.jsx
@@ -0,0 +1,28 @@
+import { FaMoon, FaSun } from "react-icons/fa";
+import {useState, useEffect} from "react";
+
+export default function Navbar(){
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() =>{
+        if (darkMode){
+            document.body.classList.add("dark-mode");
+        }else{
+            document.body.classList.remove("dark-mode")
+        }
+    }, [darkMode]);
+    return(
+        <nav className={darkMode ? "dark-mode" : "light-mode"}>
+            <h1>My Portfolio</h1>
+            <div className="nav-links">
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a href="#skils">Skills</a>
+                <a href="#contact">Contact</a>
+                <button onClick={()=> setDarkMode(!darkMode)}>
+                    {darkMode ? <FaSun size={20}/> : <FaMoon size={20}/>}
+                </button>
+            </div>
+        </nav>
+    );
+}
