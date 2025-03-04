26020efc384c2a1aad3a4bc5d113e68f11ae9921
Muli4 <mulimaurice730@gmail.com>
2025-03-04 12:56:43 +0300

    new

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Navbar from "../components/navbar";
 import Hero from "../components/hero"
-
+import Footer from "../components/footer"
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero/>
+      <Footer/>
     </div>
   );
 }
